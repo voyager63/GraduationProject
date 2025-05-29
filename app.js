4d5b28deb18513d37c4fa7910106143ff3f80e2b
voyager63 <studylee63@gmail.com>
2025-05-29 18:00:02 +0900

    Database Connected

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const db = require("./db");
+
+db.query('SELECT * FROM user', (err, results)=>{
+    if(err){
+        console.log('쿼리 에러:', err);
+        return;
+    }
+    console.log('쿼리 결과:', results);
+});
+
+
 app.get("/", (req, res)=>{
     res.status(200);
     res.send("Hello World!");
