e5931a271debf03e0e8603cc5d8e602b737d7464
voyager63 <studylee63@gmail.com>
2025-05-29 22:44:16 +0900

    sql.js Update

--- sql.js
+++ sql.js
@@ -1,9 +1,19 @@
 module.exports = {
+    userList: {
+        query: `SELECT * FROM user;`
+    },
+
     productList: {
         query: `SELECT * FROM product;`
     },
 
-    userList: {
-        query: `SELECT * FROM user;`
+    userInsert: {
+        query: `INSERT INTO user(user_id, user_password, user_email, user_phoneNumber)
+                VALUES(?, ?, ?, ?);`
+    },
+
+    productInsert: {
+        query: `INSERT INTO product(product_id, seller_id, product_name, product_price, product_quality, product_timeUsed)
+                VALUES(?, ?, ?, ?, ?, ?);`
     }
 }
