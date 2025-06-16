c08a928982321e0067f3796008a1fad0ae935c09
voyager63 <studylee63@gmail.com>
2025-06-16 16:33:51 +0900

    Modify HomeView

--- frontend/client/src/views/HomeView.vue
+++ frontend/client/src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 style="margin-bottom: 50px;">중고 전공서적 거래 시스템에 오신것을 환영합니다!</h1>
+    <h2 style="margin-bottom: 20px;">1. 상품을 둘러보세요</h2>
+    <p style="margin-bottom: 40px;">다른 사용자들이 등록한 상품을 확인할 수 있습니다.</p>
+    <h2 style="margin-bottom: 20px;">2. 메시지를 보내 보세요</h2>
+    <p style="margin-bottom: 40px;">마음에 드는 상품이 있다면, 그 상품을 등록한 사용자에게 메시지를 보내 당신의 거래 의사를 전달할 수 있습니다.</p>
+    <h2 style="margin-bottom: 20px;">3. 상품을 등록해 보세요</h2>
+    <p>당신이 판매하길 원하는 상품을 등록해 보세요. 당신의 상품을 구매하길 원하는 다른 사용자가 당신에게 메시지를 보낼 것입니다.</p>
+    <p style="margin-bottom: 40px;">당신은 그 메시지를 수락하거나 거절할 수 있습니다. 당신의 응답은 곧바로 전달됩니다.</p>
+    <h2 style="margin-bottom: 20px;">4. 거래 기록을 남겨 보세요</h2>
+    <p>당신이 구매했던 상품과 판매했던 상품이 무엇이었는지 확인할 수 있습니다.</p>
+    <p>판매자가 메시지를 수락하면, 거래 기록이 생기게 됩니다. 거래 기록은 자유롭게 삭제할 수 있습니다.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin-top: 30px;
+}
+</style>
